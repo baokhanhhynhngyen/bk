d5e05c417a44af9e6195245da66a3421c4f0c9ad
baokhanhhynhngyen <141305614+baokhanhhynhngyen@users.noreply.github.com>
2023-08-03 13:49:52 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,41 @@
+$('#slider').owlCarousel({
+loop:true,	// cho lap lại
+items:1,	// xuất hiện 1 ảnh thôi
+smartSpeed:1000,   	// tốc độ thay đổi ảnh
+autoplay:true,		// cho phép tự động chạy
+toplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+// JavaScript Document
+
+$('#slider-2').owlCarousel({
+loop:true,	// cho lap lại
+items:1,	// xuất hiện 1 ảnh thôi
+smartSpeed:1000,   	// tốc độ thay đổi ảnh
+autoplay:true,		// cho phép tự động chạy
+toplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+// JavaScript Document
+
+
+$('#slider-3').owlCarousel({
+loop:true,	// cho lap lại
+items:1,	// xuất hiện 1 ảnh thôi
+smartSpeed:1000,   	// tốc độ thay đổi ảnh
+autoplay:true,		// cho phép tự động chạy
+toplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+// JavaScript Document
+
+
+$('#slider-4').owlCarousel({
+loop:true,	// cho lap lại
+items:1,	// xuất hiện 1 ảnh thôi
+smartSpeed:1000,   	// tốc độ thay đổi ảnh
+autoplay:true,		// cho phép tự động chạy
+toplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+// JavaScript Document
